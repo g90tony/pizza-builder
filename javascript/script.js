3f21ae03542583e8ad9f921769cc5da02f276d6d
Caleb Tony <calebmbugua@gmail.com>
2021-03-10 01:44:31 +0300

    added select and bill toppings  UI triggers

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,11 @@ const newPizza = new Pizza();
 const pizzaSizeBiller = (size) => {
   newPizza.addPizzaSizeToBill(size);
 };
+
+const pizzaToppingsAdder = (topping) => {
+  newPizza.addToppingsToList(topping);
+};
+
+const pizzaToppingsBiller = () => {
+  newPizza.addToppingsToBill();
+};

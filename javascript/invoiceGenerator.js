4f9612ad44a1f2bc6f30cf461516d13d1703347b
Caleb Tony <calebmbugua@gmail.com>
2021-03-10 09:34:28 +0300

    added invoice generator

---
+++ javascript/invoiceGenerator.js
@@ -0,0 +1,45 @@
+function generateInvoice(cart) {
+  const generateInvoiceItem = (item) => {
+    const itemHTML = document.createElement("p");
+    itemHTML.innerText = item;
+    itemHTML.setAttribute("class", "my-auto stepOne-price text-white");
+
+    return itemHTML;
+  };
+
+  const { size, base, toppings } = cart;
+
+  const sizeName = size.name;
+  const sizeSubtotal = size.subtotal;
+  const baseName = base.name;
+  const baseSubtotal = base.subtotal;
+
+  document
+    .getElementById("pizzaSizeNameContainer")
+    .appendChild(generateInvoiceItem(sizeName));
+
+  document
+    .getElementById("pizzaSizeAmountContainer")
+    .appendChild(generateInvoiceItem(sizeSubtotal));
+
+  document
+    .getElementById("pizzaBaseNameContainer")
+    .appendChild(generateInvoiceItem(baseName));
+
+  document
+    .getElementById("pizzaBaseAmountContainer")
+    .appendChild(generateInvoiceItem(baseSubtotal));
+
+  toppings.forEach((topping) => {
+    const toppingsName = topping.name;
+    const toppingSubtotal = topping.subtotal;
+
+    document
+      .getElementById("pizzaToppingNameContainer")
+      .appendChild(generateInvoiceItem(toppingsName));
+
+    document
+      .getElementById("pizzaToppingAmountContainer")
+      .appendChild(generateInvoiceItem(toppingSubtotal));
+  });
+}
